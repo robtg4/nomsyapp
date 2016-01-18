ec2687e12e096e19cf5e5bf0231f9fadf7793979
Robert Greenfield <robertgreenfield@Roberts-MBP.home>
2016-01-18 00:06:26 -0500

    created front end of sign up page

--- app/screens/launch.js
+++ app/screens/launch.js
@@ -1,12 +1,22 @@
 //launch page of app
 var React = require('react-native');
-var {View, Text, StyleSheet, Image} = React;
+var {View, Text, StyleSheet, Image, TextInput} = React;
 //window size
 var Dimensions = require('Dimensions');
 var window = Dimensions.get('window');
+//components
+ImageButton = require('../components/image-button');
 
 
 module.exports = React.createClass({
+  getInitialState: function() {
+		return {
+			username: '',
+			password: '',
+			errorMessage: '',
+			loadingCurrentUser: true,
+		};
+	},
   render: function() {
     return (
       <Image
@@ -19,17 +29,43 @@ module.exports = React.createClass({
               source={require('../img/launch-logo.png')}
               style={[styles.logo]} />
           </View>
-          <View style={[styles.login, this.border('blue')]}>
-            <Text style={styles.welcome}>
-              Welcome to React Native!
-            </Text>
-            <Text style={styles.instructions}>
-              To get started, edit index.ios.js
-            </Text>
-            <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
-            </Text>
+          <View style={[styles.loginBtns, this.border('blue')]}>
+            <ImageButton
+                style={[styles.loginBtn]}
+                resizeMode={'contain'}
+                onPress={() => {}}
+                source={require('../img/facebook-btn.png')}
+                textStyle={styles.loginText}
+                text={'connect with facebook'}/>
+            <ImageButton
+                style={[styles.loginBtn]}
+                resizeMode={'contain'}
+                onPress={() => {}}
+                source={require('../img/gmail-btn.png')}
+                textStyle={styles.loginText}
+                text={'connect with google'}/>
+            <Text style={{fontFamily: 'Avenir Next', color: '5d5d5d', fontSize: 15}}>or</Text>
+          </View>
+          <View style={[styles.loginInput, this.border('orange')]}>
+            <TextInput
+              placeholder={'email address'}
+              placeholderTextColor={'#5d5d5d'}
+							style={styles.input}
+							value={this.state.username}
+							onChangeText={(text) => this.setState({username: text})} />
+            <TextInput
+              placeholder={'password'}
+              placeholderTextColor={'#5d5d5d'}
+							style={styles.input}
+							value={this.state.password}
+							onChangeText={(text) => this.setState({password: text})} />
+            <ImageButton
+                style={[styles.loginBtn, {marginTop: 20}]}
+                resizeMode={'contain'}
+                onPress={() => {}}
+                source={require('../img/sign-in-btn.png')}
+                textStyle={styles.signinText}
+                text={'connect with email'}/>
           </View>
         </View>
       </Image>
@@ -37,28 +73,67 @@ module.exports = React.createClass({
   },
   border: function(color) {
 	    return {
-	      borderColor: color,
-	      borderWidth: 2,
+	      //borderColor: color,
+	      //borderWidth: 2,
 	    }
 	 },
 });
 
 var styles = StyleSheet.create({
-  logoTop: {
+  input: {
+    color: '#5d5d5d',
+		padding: 4, //gives us offset to border
+		height: window.height/18,
+		backgroundColor: 'rgba(255,255,255, 0.8)',
+		borderColor: 'rgba(255,255,255, 0.8)',
+    fontFamily: 'Avenir Next',
+		borderRadius: 5, //round input box
+		margin: 2,
+		width: window.width/1.5,
+		alignSelf: 'center', //center yourself on form when you have fixed widths
+	},
+  loginInput: {
     flex: 2,
+    width: window.width,
+    alignItems: 'center'
+  },
+  signinText: {
+    fontSize: 13,
+    color: 'white',
+    fontFamily: 'Avenir Next',
+    alignSelf: 'center',
+  },
+  loginText: {
+    fontSize: 13,
+    color: 'white',
+    fontFamily: 'Avenir Next',
+    marginLeft: window.width/20,
+  },
+  loginBtn: {
+    width: window.width/1.6,
+    height:(156/739)*window.width/1.6,
+    margin: 2,
+    justifyContent: 'center',
+  },
+  logoTop: {
+    flex: 1.5,
     alignItems: 'center',
     width: window.width,
     justifyContent: 'space-around',
+    flexDirection: 'column'
   },
-  login: {
-    flex: 3,
+  loginBtns: {
+    flex: 1,
     width: window.width,
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    alignItems: 'center',
   },
   logo: {
-    justifyContent: 'center',
-    width: window.width/1.4,
-    height: (481/816)*window.width/1.4,
+    marginTop: (window.width/1.6)*0.30,
+    justifyContent: 'flex-end',
+    width: window.width/1.6,
+    height: (481/816)*window.width/1.6,
     alignSelf: 'center',
   },
   bg: {
